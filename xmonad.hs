5f5f0a1b720bc1afc9909e7ceb5c3f42dd0cfb35
Pedro Vanzella <pedro@pedrovanzella.com>
2012-05-04 17:11:54 -0300

    adding space for a dock

--- xmonad.hs
+++ xmonad.hs
@@ -1,7 +1,15 @@
 -- default desktop configuration for Fedora
 
 import XMonad
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.ManageDocks
+import XMonad.Util.Run(spawnPipe)
+import XMonad.Util.EZConfig(additionalKeys)
+import System.IO
 
-main = xmonad defaultConfig
-	{  terminal = "terminator"
-	}
+main = do
+	xmonad $ defaultConfig
+		{ terminal = "terminator"
+		, manageHook = manageDocks <+> manageHook defaultConfig
+		, layoutHook = avoidStruts  $  layoutHook defaultConfig
+		}
