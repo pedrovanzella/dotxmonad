44cbb7f8d97784aabac84bc2cce64135ad3b22cd
Pedro Vanzella <pedro@pedrovanzella.com>
2012-06-12 15:13:25 -0300

    printscreen tirando screenshot, ctrl + printscreen tirando da janela

--- xmonad.hs
+++ xmonad.hs
@@ -23,4 +23,6 @@ main = do
 						}
 		} `additionalKeys`
 		[ ((mod4Mask .|. shiftMask, xK_z), spawn "xscreensaver-command -lock")
+		, ((controlMask, xK_Print), spawn "sleep 0.2; scrot -s")
+		, ((0, xK_Print), spawn "scrot")
 		]
